1210242bbd61041928dc4e6896eb9eea4c252a21
Hethe Berg <growthcode@gmail.com>
2014-09-27 23:49:59 -0700

    init commit, updated gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PapOnRails::Application.config.secret_token = 'dc0334d5da7d048df9caecc6ca58c0dd11e10f183d11ecfdd167dd9856d15c007081634f6bee8073ed65af3f07db518e9aa1619afd1d93694da365dbb5a103ba'
