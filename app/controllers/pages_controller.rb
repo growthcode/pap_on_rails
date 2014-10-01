02f9bfeaae53a263893c5bda0c934ebe6b0c7c44
Hethe Berg <growthcode@gmail.com>
2014-09-30 18:27:08 -0700

    removed show views and actions, updated links, added a little bootstrap inline html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
   def home
+    if user_signed_in?
+      redirect_to projects_path
+    end
   end
 
   def about
