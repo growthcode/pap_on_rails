fd38c31496e13008e0013445e3e09772ff18c421
Hethe Berg <growthcode@gmail.com>
2014-12-03 17:09:56 -0800

    added buttons and count methods on pap_model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,17 @@ class Project < ActiveRecord::Base
   validates :project_name, :presence => true
 
   attr_accessible :project_name
+
+
+  def number_of_pap_actions
+    pap_actions.count
+  end
+  
+  def number_of_state_of_action_complete
+    pap_actions.where(state_of_action: "complete").count
+  end
+  
+  def number_of_state_of_action_not_ready
+    pap_actions.where(state_of_action: "not ready").count
+  end
 end
